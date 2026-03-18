8ccab9d91975f85d81c3c7579fc91a09209d33fd
Gargi001-source <gargi.joshe@gmail.com>
2026-03-18 14:55:16 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ module.exports = function() {
   app.use(getProfile);
 
   /* istanbul ignore next */
-  app.get('/hi', (req, res) => {
+  res.send({ message: "Hello from my CI/CD pipeline!" })
     res.status(200).send('Hi there!');
   });
 
